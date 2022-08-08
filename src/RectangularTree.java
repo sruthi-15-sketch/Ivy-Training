3dfb10a2ac131abee4a32cd5c4010c3beba333be
sruthi-15-sketch <saisiva1715@gmail.com>
2022-08-08 20:34:45 +0530

    Sequence Program

---
+++ src/RectangularTree.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class RectangularTree {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int row=sc.nextInt();
+        int col=sc.nextInt();
+        int m=sc.nextInt();
+        if (m-1%col==0 || m<=col || row-1*col>m){
+            System.out.println("Mango Tree");
+        }
+        else{
+            System.out.println("Not a Mango Tree");
+        }
+    }
+}
