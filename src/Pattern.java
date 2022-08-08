cd12cd74b89c9727b11ec9bf23b3837db31ee007
sruthi-15-sketch <saisiva1715@gmail.com>
2022-08-09 00:02:02 +0530

    Pattern Program

--- src/Pattern.java
+++ src/Pattern.java
@@ -4,12 +4,14 @@ public class Pattern
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
+        char a=sc.next().charAt(0);
+        char b=sc.next().charAt(0);
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
-                System.out.print("*");
+                System.out.print(a+" ");
             }
             for(int k=n-i;k>0;k--){
-                System.out.print("#");
+                System.out.print(b+" ");
             }
             System.out.println();
         }
