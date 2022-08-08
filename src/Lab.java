b7f78795a078d158ef4be3be0fc50fbe3e7db4b0
Siva Sruthi Boddu <Sruthi.Boddu@ivycomptech.com>
2022-08-08 15:44:23 +0530

    Lab Systems Code

---
+++ src/Lab.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Lab {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=sc.nextInt();
+        int n=sc.nextInt();
+        int res=n-x;
+        if(res<n-y && n-y<0){
+            System.out.println("L2");
+            res=n-y;
+        }
+        else if(res<n-z && n-z<0){
+            System.out.println("L3");
+        }
+        else{
+            System.out.println("L1");
+        }
+
+    }
+}
