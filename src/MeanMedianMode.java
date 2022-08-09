ab5ee27e048a87d6c62ffdb103a9f9fd4751a7b3
sruthi-15-sketch <saisiva1715@gmail.com>
2022-08-09 13:28:18 +0530

    Median part push

--- src/MeanMedianMode.java
+++ src/MeanMedianMode.java
@@ -10,6 +10,14 @@ public class MeanMedianMode {
             sum+=arr[i];
         }
         System.out.println("Mean: "+(sum/n));
+        Arrays.sort(arr);
+        if(n%2!=0){
+            System.out.println("Median: "+arr[((n+1)/2)-1]);
+        }
+        else {
+            float median=(float)((n/2)+((n/2)+1))/2;
+            System.out.println("Median: " + median);
+        }
 
     }
 }
